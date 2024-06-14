b864c52eed1b411b28b05cc2c8131ec376704550
Josh Hackney <jhaxllc@gmail.com>
2024-06-14 15:04:05 -0400

    added solution that uses combinatory math, which runs quite a bit faster than the previous solution at O(M) time

--- gridtraveler/gridTraveler.go
+++ gridtraveler/gridTraveler.go
@@ -26,3 +26,11 @@ func UniquePaths(m int, n int) int {
 
 	return result
 }
+
+func UniquePathsCombinatory(m int, n int) int {
+	ans := 1
+	for i := 1; i <= m-1; i++ {
+			ans = ans * (n - 1 + i) / i
+	}
+	return ans
+}
--- main.go
+++ main.go
@@ -7,12 +7,25 @@ import (
 	"github.com/jay13jay/metaPractice/gridtraveler"
 )
 
+const end = 10000000000
+const start = end - 3
 func main() {
-	for i := 1000; i <= 10000; i+=2 {
+	// fmt.Println("DP:")
+	// for i := start; i <= end; i+=2 {
+	// 	start := time.Now()
+	// 	// fmt.Printf("Grid: %dX%d\tPaths: %d\n", i, i, gridtraveler.UniquePaths(i, i) )
+	// 	_ = gridtraveler.UniquePaths(i, i)
+	// 	end := time.Now()
+	// 	diff := end.Sub(start)
+	// 	fmt.Printf("Paths: %d\tTime: %s\n", i, diff)
+	// }
+	fmt.Println("\nCombinatory:")
+	for i := start; i <= end; i++ {
 		start := time.Now()
-		fmt.Printf("Grid: %dX%d\tPaths: %d\n", i, i, gridtraveler.UniquePaths(i, i) )
+		// fmt.Printf("Grid: %dX%d\tPaths: %d\n", i, i, gridtraveler.UniquePathsCombinatory(i, i) )
+		_ = gridtraveler.UniquePathsCombinatory(i, i)
 		end := time.Now()
 		diff := end.Sub(start)
-		fmt.Printf("Time taken: %s\n", diff)
+		fmt.Printf("Grid:%d X %d\tTime: %s\n", i, i, diff)
 	}
 }
