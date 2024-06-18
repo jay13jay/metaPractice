7250949dfa71143fc9d92bd8886c1e8a05681f30
Josh Hackney <jhaxllc@gmail.com>
2024-06-18 00:40:32 -0400

    added performance testing code, plus put numbers into readme

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/jay13jay/metaPractice/mergesort"
 )
 
+var size int = 10000000
 func main() {
 	// ret := combosum.ComboSum([]int{2, 3, 6, 7}, 7)
 	// ret := combosum.ComboSum([]int{2, 3, 5}, 0)
-	arr := []int{5,70,11,11,26,28,83,34,35,14,19,22}
-	res := mergesort.MS(arr)
-
-	fmt.Printf("\nResults:\n%v\n", res)
+	arr := make([]int, size)
+	for i := 0; i < size; i++ {
+		arr = append(arr, int(rand.Intn(size)))
+	}
+	for t := 0; t < 5; t++ {
+		start := time.Now()
+		_ = mergesort.MS(arr)
+		end := time.Now()
+		tDiff := end.Sub(start)
+		fmt.Printf("Arr size: %d, Time: %s\n", size, tDiff)
+		size *= 2
+	}
 }
