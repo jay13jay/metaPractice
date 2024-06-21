6a9a8511c3f29e31e69ed937eea317d3774e83ee
Josh Hackney <jhaxllc@gmail.com>
2024-06-21 14:58:11 -0400

    sovled the boss fight challenge, believe the solution is O(n*log(n)) but I'm just backhand estimating

--- bossfight/bossFight.go
+++ bossfight/bossFight.go
@@ -1,8 +1,53 @@
 package bossfight
 
+// var N int32 = 3													// # of fighters
+// var H []int32 = []int32{2, 1, 4} 	// Health
+// var D []int32 = []int32{3, 1, 2} 		// Fighter Damage / sec
+// var B int32 = 4 												// Boss Damage / sec
+
+
+	
 func getMaxDamageDealt(N int32, H []int32, D []int32, B int32) float64 {
   // Write your code here
-  return 0.0
+	var maxTotalDamage float64 = 0
+
+	// Maps to memoize the seconds and damages each warrior can last and deal
+	secondsMap := make(map[int]float64)
+	damageMap := make(map[int]float64)
+
+	// Calculate damage for each combination of frontline and rearguard positions
+	for i := 0; i < int(N); i++ {
+		frontlineSeconds, exists := secondsMap[i]
+		if !exists {
+			frontlineSeconds = float64(H[i]) / float64(B)
+			secondsMap[i] = frontlineSeconds
+		}
+		frontlineDamage, exists := damageMap[i]
+		if !exists {
+			frontlineDamage = float64(D[i]) * frontlineSeconds
+			damageMap[i] = frontlineDamage
+		}
+		for j := 0; j < int(N); j++ {
+			if i != j {
+				rearguardSeconds, exists := secondsMap[j]
+				if !exists {
+					rearguardSeconds = float64(H[j]) / float64(B)
+					secondsMap[j] = rearguardSeconds
+				}
+				rearguardDamage := float64(D[j]) * (frontlineSeconds + rearguardSeconds)
+
+				// Total damage
+				totalDamage := frontlineDamage + rearguardDamage
+
+				// Update maximum total damage if the current combination deals more damage
+				if totalDamage > maxTotalDamage {
+					maxTotalDamage = totalDamage
+				}
+			}
+		}
+	}
+
+	return maxTotalDamage
 }
 
 func GetDamage(N int32, H []int32, D []int32, B int32) float64 {
--- main.go
+++ main.go
@@ -2,15 +2,42 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/jay13jay/metaPractice/bossfight"
 )
+var size int32 = 1000
+var source rand.Source = rand.NewSource(time.Now().UnixNano())
+var rng *rand.Rand = rand.New(source)
 
 func main() {
-	var N int32 = 3
-	var H []int32 = []int32{1, 1, 2, 100}
-	var D []int32 = []int32{1, 2, 1, 3}
-	var B int32 = 8
-	ret := bossfight.GetDamage(N, H, D, B)
-	fmt.Printf("Damage: %.2f", ret)
+	for i := 0; i < 10; i ++ {
+		// Health
+		var H []int32 = createSlice(size, 10)
+		// var H []int32 = []int32{1, 1, 2, 100}
+		N := size
+
+		// Fighter Damage / sec
+		// var D []int32 = []int32{1, 2, 1, 3}
+		D := createSlice(size, 10)
+		// Boss Damage / sec
+		var B int32 = int32(rng.Intn(20))
+
+		start := time.Now()
+		ret := bossfight.GetDamage(N, H, D, B)
+		end := time.Now()
+		elapsed := end.Sub(start)
+		fmt.Printf("Fighters: %d \t Damage: %.2f\tTime: %v\n", N, ret, elapsed)
+		
+		size += 1000
+	}
+}
+
+func createSlice(size int32, card int) []int32 {
+	arr := make([]int32, size)
+	for i := 0; i < int(size); i++ {
+		arr[i] = int32(rng.Intn(card))
+	}
+	return arr
 }
