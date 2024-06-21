fe25d5e40ad3d9efd435c5efb644a558b59d20c1
Josh Hackney <jhaxllc@gmail.com>
2024-06-21 00:16:28 -0400

    solved hops challenge, I believe it's O(N) time with only 1 if so a small constant

--- hops/getPadHops.go
+++ hops/getPadHops.go
@@ -1,8 +1,27 @@
 package hops
 
+/*
+N: 6 - 			# of pads
+F: 3 - 			# of frogs
+P: 5, 2, 4 	# occuppied pads
+Pads:
+1: false
+2: true
+3: false
+4: true
+5: true
+6: false
+*/
 func getSecondsRequired(N int64, F int32, P []int64) int64 {
   // Write your code here
-  return 0
+	frog1 := P[0]
+	for i := 1; i < int(F); i++ {
+		if P[i] < frog1 {
+			frog1 = P[i]
+		}
+	}
+
+  return N - frog1
 }
 
 func SolveHops(N int64, F int32, P []int64) int64 {
--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	var N int64 = 6
-	var F int32 = 3
-	P := []int64{5, 2, 4}
+	var N int64 = 6 	// Number of pads
+	var F int32 = 3 	// Number of frogs
+	P := []int64{5, 2, 4} 	// Occupied pads
 	
 	ret := hops.SolveHops(N, F, P)
 	fmt.Println(ret)
