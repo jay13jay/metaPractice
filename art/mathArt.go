91014605d9d8381cf0c722930d8af48b31ddb38c
Josh Hackney <jhaxllc@gmail.com>
2024-06-25 13:08:33 -0400

    initial math art setup

---
+++ art/mathArt.go
@@ -0,0 +1,12 @@
+package art
+
+func getPlusSignCount(N int32, L []int32, D string) int64 {
+  // Write your code here
+	var ret int64 = 0
+	
+  return ret
+}
+
+func Solve(N int32, L []int32, D string) int64 {
+	return getPlusSignCount(N, L, D)
+}
