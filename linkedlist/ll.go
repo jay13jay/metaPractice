90246761fd4a3f5345c2c4c813b7623467764264
Josh Hackney <jhaxllc@gmail.com>
2024-06-25 16:02:08 -0400

    created linked list package

---
+++ linkedlist/ll.go
@@ -0,0 +1,121 @@
+package linkedlist
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Linked List struct. Contains pointer to Head, and Items, containing # of elements
+type List struct {
+	Head *Node
+	Items int
+}
+
+// a linked list node. Contains the element, Data, and a pointer to the next node, Next
+type Node struct {
+	Data int
+	Next *Node
+}
+
+// Append an element to the end of the list
+func (l *List) Append(d int) {
+	node := &Node{Data: d}
+
+	// Check if the list is empty
+	if l.Head == nil {
+		l.Head = node
+	} else {
+		current := l.Head
+		for current.Next != nil {
+			current = current.Next
+		}
+		current.Next = node
+	}
+	l.Items++
+}
+
+// Add an element at the beginning of the list
+func (l *List) Prepend(d int) {
+	node := &Node{Data: d}
+	node.Next = l.Head
+	l.Head = node
+
+	l.Items++
+}
+
+// Insert an element into the list after a node.
+// Takes int to insert, and *Node to insert after
+func (l *List) Insert(d int, n *Node) {
+	next := n.Next
+	node := &Node{Data: d}
+	n.Next =  node
+	node.Next = next
+	l.Items++
+}
+
+// Takes *Node as argument, Removes the node after it
+func (l *List) Delete(n *Node) {
+	n.Next = n.Next.Next
+	l.Items--
+}
+
+// Delete first node containing provided value
+func (l *List) DeleteValue(d int) error {
+	var prev *Node
+	current := l.Head
+	for current.Next != nil {
+		if current.Data == d {
+			break
+		}
+		prev = current
+		current = current.Next
+	}
+	if current.Data == d {
+		prev.Next = current.Next
+		return nil
+	} else {
+		err := fmt.Sprintf("item not found: %d\n", d)
+		return errors.New(err)
+	}
+
+}
+
+func (l *List) Length() int {
+	return l.Items
+}
+
+// Reverses the linked list
+func (l *List) Reverse() {
+
+}
+
+// Returns a pointer to the first node containing provided value
+func (l *List) Search(d int) *Node {
+	current := l.Head
+	for current.Data != d {
+		current = current.Next
+	}
+	if current.Data == d {
+		return current
+	} else {
+		return nil
+	}
+}
+
+func (l *List) isEmpty() bool {
+	if l.Head == nil {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (l *List) Print() {
+	current := l.Head
+	fmt.Printf("Items: %d | ", l.Items)
+	for current.Next != nil {
+		fmt.Printf("%d -> ", current.Data)
+		current = current.Next
+	}
+	fmt.Printf("%d\n", current.Data)
+}
--- main.go
+++ main.go
@@ -3,21 +3,28 @@ package main
 import (
 	"fmt"
 
-	"github.com/jay13jay/metaPractice/tunnel"
+	"github.com/jay13jay/metaPractice/linkedlist"
 )
 
 func main() {
-	// var C int64 = 10
-	// var N int32 = 2
-	// var A []int64 = []int64{1, 6}
-	// var B []int64 = []int64{3, 7}
-	// var K int64 = 7
-	var C int64 = 50
-	var N int32 = 3
-	var A []int64 = []int64{39, 19, 28}
-	var B []int64 = []int64{49, 27, 35}
-	var K int64 = 15
-	ret := tunnel.Solve(C, N, A, B, K)
-	fmt.Println("Time required: ", ret)
-	
+	list := &linkedlist.List{}
+
+	for i := 0; i < 10; i++ {
+		list.Append(i)
+	}
+	insertNode := list.Head
+	for i := 0; i <= 4; i++ {
+		insertNode = insertNode.Next
+	}
+	list.Insert(75, insertNode)
+
+	list.Prepend(51)
+	list.Delete(insertNode.Next)
+	err := list.DeleteValue(100)
+	if err != nil {
+		fmt.Printf("Could not delete\nError: %v", err)
+	}
+	_ = list.DeleteValue(4)
+
+	list.Print()
 }
