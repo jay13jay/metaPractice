e8151ef4c0077315cb8819c1c753df0f3e868c19
Josh Hackney <jhaxllc@gmail.com>
2024-06-25 13:07:52 -0400

    initial conveyor setup

---
+++ conveychaos/conveyor.go
@@ -0,0 +1,11 @@
+package conveychaos
+
+func getMinExpectedHorizontalTravelDistance(N int32, H []int32, A []int32, B []int32) float64 {
+  // Write your code here
+	var ret float64 = 0.0
+
+	// get tunnel lengths
+
+
+  return ret
+}
